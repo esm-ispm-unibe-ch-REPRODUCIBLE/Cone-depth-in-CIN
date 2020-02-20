94e344c4314b23c12f2f285953a4fce1496a2a51
georgiasalanti <georgia.salanti@gmail.com>
2020-02-20 15:54:45 +0100

    first commit

---
+++ MASTER.R
@@ -0,0 +1,13 @@
+#First you need to install the following packages:   
+install.packages("meta")
+install.packages("metafor")
+install.packages("readxl") 
+install.packages("dosresmeta")
+install.packages("rms") 
+#load libraries
+library(netmeta)
+library(meta)
+library(metafor)
+library(readxl)
+library(dosresmeta)
+library(rms)
